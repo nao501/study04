5c670356de265f97648c552c42dc0bb12280c6db
nao501 <79750877+nao501@users.noreply.github.com>
2021-04-10 13:52:45 +0900

    Add files via upload

--- pos-system.py
+++ pos-system.py
@@ -1,3 +1,6 @@
+from numpy.lib.twodim_base import mask_indices
+import pandas as pd
+import csv
 ### 商品クラス
 class Item:
     def __init__(self,item_code,item_name,price):
@@ -27,32 +30,55 @@ def main():
 
     # マスタ登録
     item_master=[]
-    item_master.append(Item("001","りんご",100))
-    item_master.append(Item("002","なし",120))
-    item_master.append(Item("003","みかん",150))
+    #商品マスター.csvからデータを読み込み
+    master_deta = open("商品マスター.csv",'r')
+    a_list = []
+    b_list = []
+    c_list = []
+
+    for row in csv.reader(master_deta):
+        a_list.append(row[0])
+        b_list.append(row[1])
+        c_list.append(row[2])
+    
+    #商品マスター.csvの1行目のセルデータを削除
+    del a_list[0]
+    del b_list[0]
+    del c_list[0]
+
+    #各リストをItem()に入れていく
+    for item_code, item_name, price in zip(a_list,b_list,c_list):
+        item_master.append(Item(item_code, item_name, price))
+
+
+    #item_master.append(Item("001","りんご",100))
+    #item_master.append(Item("002","なし",120))
+    #item_master.append(Item("003","みかん",150))
     
     # オーダー登録
     order=Order(item_master)
     order.add_item_order("001")
     order.add_item_order("002")
     order.add_item_order("003")
-    item1 = Item("001","りんご",100)
-    item2 = Item("002","なし",120)
-    item3 = Item("003","みかん",150)
-# マスター検索
-    if item1.item_code in '001':
-        print(item1.item_code +":"+item1.item_name,item1.price)
-    else:
-        print("miss")
-
-   # 元マスター検索
-    #order_list = sorted(item_master,key = lambda x:x.item_code,reverse=False)
-    #for o in order_list[list(map(lambda x:x.item_code,order_list)).index("001"):]:
-    #   print(o.item_code)
-
+    order.add_item_order("004")
     # オーダー表示
     order.view_item_list()
+    # オーダー登録
+    order_code = input("商品コードを入力してください：")
     
-    
+    # マスター検索
+    for item in item_master:
+        for item_order in order.item_order_list:
+            if item.item_code == item_order:
+                print("商品コード" +item.item_code +":"+ item.item_name + '￥'+str(item.price)+'円')
+    for item in item_master:
+        if item.item_code == order_code:
+            print("\n選んだ商品は商品コード" +item.item_code +":"+ item.item_name + '￥'+str(item.price)+'円')
+              
+            
+  
+   
+
+
 if __name__ == "__main__":
     main()
